0c98b528f7e06dec24f24a61fe7034297b654c8a
vutaphuong <33868068+vutaphuong@users.noreply.github.com>
2017-12-02 16:40:20 +0700

    Trang chủ của giảng viên

---
+++ trangchugiangvien.php
@@ -0,0 +1,76 @@
+
+<?php
+if (!isset($_SESSION)) 
+  {
+    session_start();
+  }
+if (isset($_SESSION['user'])) {
+  
+ $tennd=$_SESSION['user'];
+}  
+
+if (!isset($_SESSION['user'])) {
+  
+ header('Location: index.php');
+}
+?>
+<!doctype html>
+<html>
+<head><meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+
+<title>Đăng ký môn học STUonline</title>
+    <link href="css/hover.css" rel="stylesheet" media="all">
+     <link href="css/magic.css" rel="stylesheet">
+     <link href="css/logoheader.css" rel="stylesheet">
+     <link href="css/dinhdangnut.css" rel="stylesheet">
+     <!--<link href="css/Untitled-3.css" rel="stylesheet">-->
+
+<script language=JavaScript>
+    var txt=" Đăng ký môn học STUonline";
+    var expert=500;
+    // speed of roll
+    var refresh=null;
+    function marquee_title(){
+    document.title=txt;
+    txt=txt.substring(1,txt.lenghth)+txt.charAt(0);
+    refresh=setTimeout("marquee_title()",expert);
+    }
+    marquee_title();
+    </script>
+    <!--icon-->
+    <link href="image/amarok.png" rel="icon" />
+
+      
+</head>
+<body >
+    <div id="header">
+    <table align="center" class="tb">
+        <tr class="logo"><td class="logophai stu1">S</td><td class="logotrai stu2">T</td><td class="logophai stu1">U</td><td class="logotrai">o</td><td class="logophai">n</td><td class="logotrai">l</td><td class="logophai">i</td><td class="logotrai">n</td><td class="logophai">e</td></tr>
+        </table>
+        </div>
+        
+    <div id="main">
+    <div id="thanweb">
+    <div id="menutrai">
+    <ul>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a href="trangchu.php">Trang chủ</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a>Thông báo ĐKMH</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a>Quy định ĐKMH</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a href="xemlichday.php">Xem lịch dạy</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a href="thongtin.php">Thông tin cá nhân</a></li>
+    </ul>
+    </div>
+    <div id="noidung">  
+    <iframe src="https://drive.google.com/file/d/1TU6l5faustXDL5Vt6BvOdMl7zhWXPCNz/preview" width="900" height="700"></iframe>
+    </div>
+    </div>    
+  </div>
+  <!--Chân web -->
+  <div id="footer">
+  <table align="center" style="padding-top:10px"><tr><td>
+  DESIGN by Nguyễn Thế Mạnh &amp; Vũ Tá Phương
+  </td></tr></table>
+  </div>
+            
+</body>
+</html>
