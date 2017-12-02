5a5bf684e9e5c0714da6550597e282b3cbe32974
vutaphuong <33868068+vutaphuong@users.noreply.github.com>
2017-12-02 16:39:35 +0700

    Xem lịch dạy của giảng viên

--- xemlichday.php
+++ xemlichday.php
@@ -1,14 +1,8 @@
-
 <?php
-if (!isset($_SESSION)) 
-  {
-    session_start();
-  }
-
-if (!isset($_SESSION['tenuser'])) {
-  
- //header('Location: index.php');
-}
+  include 'config/kiemtra_SESION.php';
+  include 'classes/config.php';
+  include 'classes/function.php';
+  spl_autoload_register("loadClass");
 ?>
 <!doctype html>
 <html>
@@ -19,6 +13,7 @@ if (!isset($_SESSION['tenuser'])) {
      <link href="css/magic.css" rel="stylesheet">
      <link href="css/logoheader.css" rel="stylesheet">
      <link href="css/dinhdangnut.css" rel="stylesheet">
+
      <!--<link href="css/Untitled-3.css" rel="stylesheet">-->
 
 <script language=JavaScript>
@@ -39,6 +34,10 @@ if (!isset($_SESSION['tenuser'])) {
       
 </head>
 <body >
+    <?php
+      $obj= new Db();
+      $rows = $obj->select("SELECT hoten from giangvien where `magv`='$tennd'");
+    ?>
     <div id="header">
     <table align="center" class="tb">
         <tr class="logo"><td class="logophai stu1">S</td><td class="logotrai stu2">T</td><td class="logophai stu1">U</td><td class="logotrai">o</td><td class="logophai">n</td><td class="logotrai">l</td><td class="logophai">i</td><td class="logotrai">n</td><td class="logophai">e</td></tr>
@@ -49,19 +48,36 @@ if (!isset($_SESSION['tenuser'])) {
     <div id="thanweb">
     <div id="menutrai">
     <ul>
-    <li class="hvr-sweep-to-right hvr-ripple-out"><a>Trang chủ</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a href="trangchugiangvien.php">Trang chủ</a></li>
     <li class="hvr-sweep-to-right hvr-ripple-out"><a>Thông báo ĐKMH</a></li>
     <li class="hvr-sweep-to-right hvr-ripple-out"><a>Quy định ĐKMH</a></li>
-    <li class="hvr-sweep-to-right hvr-ripple-out"><a>Đăng ký môn học</a></li>
-    <li class="hvr-sweep-to-right hvr-ripple-out"><a>Thông báo học phí</a></li>
-    <li class="hvr-sweep-to-right hvr-ripple-out"><a href="thongtinnv.php">Thông tin cá nhân</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out"><a href="xemlichday.php">Xem lịch dạy</a></li>
+    <li class="hvr-sweep-to-right hvr-ripple-out">Thông tin cá nhân</li>
     </ul>
     </div>
-    <div id="noidung">    
-    <p style="color: pink; font-weight: bold;font-size: 20px">Đây là trang Xem lịch dạy của giáo viên</p>
-    </div>
-    </div>    
-  </div>
+      <div id="noidung">
+        <form action="xulythongtindangky.php" method="post"> 
+          <table border="1" cellpadding="1" cellspacing="1" align="center" width="900px">
+                <tr>
+                  <td colspan="10" align="center" style="font-family: 'Comic Sans MS';font-size: 30px;color: blue;">Lịch dạy của Giảng viên:
+                    <?php 
+                      foreach ($rows as $row) {
+                        # code...
+                        echo $row['hoten'];
+                      }
+                    ?>
+                  </td>
+                </tr>
+                <tr>
+                <td align="center">Mã giảng viên</td>
+                <td align="center">Tên giảng viên</td>
+                <td align="center">Tên môn học</td>
+                <td align="center">Tiết dạy</td>
+                <td align="center">Ngày dạy</td>
+                <td align="center">Phòng</td>
+                </tr> 
+            </table>  
+      </div>
   <!--Chân web -->
   <div id="footer">
   <table align="center" style="padding-top:10px"><tr><td>
